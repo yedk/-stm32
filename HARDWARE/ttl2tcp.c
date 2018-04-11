49f634b37524b3dcd1ed986d70ebc84e37cbfcda
ye <2360231009@qq.com>
2018-04-11 11:37:02 +0800

    1.包头为0x04

--- HARDWARE/ttl2tcp.c
+++ HARDWARE/ttl2tcp.c
@@ -26,7 +26,8 @@ void Send2Pc(char * buf,char len)
 
 void tcpcmd()
 {
-	char ack[6]={0x00,0x04,0x00,0x0A,0x00,0x00};
+	char ack[8]={0x00,0x04,0x00,0x0A,0x00,0x00,0x01,0x0f};
+	char ack1[8]={0x00,0x04,0x00,0x0D,0x00,0x00,0x01,0x0f};
 	//char len;
 //	char t=0;
 	//char a =-1;
@@ -37,7 +38,7 @@ void tcpcmd()
 	{
 		if((USART_RX_BUF[1]==0x04)&&(USART_RX_BUF[2]==0x00)&&(USART_RX_BUF[3]==0x00)&&(USART_RX_BUF[4]==0x00)&&(USART_RX_BUF[5]==0x00))
 		{
-			
+			Send2Pc(ack,8);
 			INFORM=1;//闸机开启标志；
 			USART_RX_BUF[5]=0;
 			USART_RX_STA=0;
@@ -45,11 +46,11 @@ void tcpcmd()
 			BEEP=1;
 			delay_ms(1000);
 			BEEP=0;
-			Send2Pc(ack,6);
+			Send2Pc(ack1,8);
 		}
 		else if((USART_RX_BUF[2]==0x00)&&(USART_RX_BUF[3]==0x01))
 		{
-			//Send2Pc(ack,6);
+			Send2Pc(ack,8);
 			for(i=0;i<=(USART_RX_BUF[1]-4);i++)
 			{
 				voice1[i]=USART_RX_BUF[(i+6)];
@@ -60,17 +61,17 @@ void tcpcmd()
 			XFS_FrameInfo(voice1,(USART_RX_BUF[1]-4));
 			//XFS_FrameInfo(voice1,4);
 			//
-			Send2Pc(ack,6);
+			Send2Pc(ack1,8);
 		}
 		else if((USART_RX_BUF[1]==0x04)&&(USART_RX_BUF[2]==0x00)&&(USART_RX_BUF[3]==0x00)&&(USART_RX_BUF[4]==0x00)&&(USART_RX_BUF[5]==0x01))
 		{
-			
+			Send2Pc(ack,8);
 			INFORM=1;//闸机开启标志；
 			//BEEP=1;
 			//delay_ms(1000);
 			//BEEP=0;
 			USART_RX_BUF[5]=0;
-			Send2Pc(ack,6);
+			Send2Pc(ack1,6);
 		}
 		USART_RX_STA=0;//接收标志清零；
 	}

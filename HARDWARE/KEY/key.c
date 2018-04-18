af60a813deacb03da6dd53864087ff135ad4287c
ye <2360231009@qq.com>
2018-04-18 19:11:36 +0800

    11

--- HARDWARE/KEY/key.c
+++ HARDWARE/KEY/key.c
@@ -23,7 +23,7 @@ void KEY_Init(void) //IO初始化
  	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOE,ENABLE);//使能PORTA,PORTE时钟
 
 	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_3|GPIO_Pin_9;//出门闸机1和出门闸机2；
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; //设置成上拉输入
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; //设置为下拉输入；
  	GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIOE2,3,4
 
 }
@@ -40,12 +40,12 @@ u8 KEY_Scan(u8 mode)
 {	 
 	static u8 key_up=1;//按键按松开标志
 	if(mode)key_up=1;  //支持连按		  
-	if(key_up&&(KEY0==0||KEY1==0))
+	if(key_up&&(KEY0==1||KEY1==1))
 	{
-		delay_ms(10);//去抖动 
+		delay_ms(160);//去抖动 
 		key_up=0;
-		if(KEY0==0)return KEY0_PRES;
-		else if(KEY1==0)return KEY1_PRES;
-	}else if(KEY0==1&&KEY1==1)key_up=1; 	    
+		if(KEY0==1)return KEY0_PRES;
+		else if(KEY1==1)return KEY1_PRES;
+	}else if(KEY0==0&&KEY1==0)key_up=1; 	    
  	return 0;// 无按键按下
 }
